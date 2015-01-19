ad2c510b0e5789c0c1135db7383845e9f3cbc7c4
TheFraspi <spier084@gmail.com>
2015-01-20 00:40:27 +0100

    Nothing serious

--- PositioningEngine.py
+++ PositioningEngine.py
@@ -1,1 +1,14 @@
 __author__ = 'spier_000'
+
+
+
+
+def main():
+    """Main loop of the Positioning Engine of the broom"""
+    def __init__(self):
+        x=0
+
+
+
+if __name__ == '__main__':
+    main()
