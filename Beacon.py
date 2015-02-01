6846d811aae95ffc9a5b23829ba85c7d01f1d551
TheFraspi <spier084@gmail.com>
2015-02-01 16:31:05 +0100

    Nothing serious

--- Beacon.py
+++ Beacon.py
@@ -1,11 +1,8 @@
 __author__ = 'spier_000'
 
-class Position:
-    """"This class represent the Position of an object"""
+class beacon:
     def __init__(self, x, y, angle, name):
         self.x = x
         self.y = y
         self.angle = angle
         self.name = name
-
-
--- Broom.py
+++ Broom.py
@@ -1,5 +1,38 @@
 __author__ = 'spier_000'
 
-class Broom:
-    def __init__(self):
-        self.name = "Lol"
+import Position
+import Beacon
+import time
+import math
+
+
+b1x = 0
+b1y = 0
+b2x = 2
+b2y = 0
+b3x = 2
+b3y = 3
+angle_a = 30
+angle_b = 290
+angle_c = 255
+
+def calc_velocity(self, position1, position2):
+    #   blabla
+    self.pos1 = position1
+    self.pos2 = position2
+    velocity = 0
+
+def calc_position(self, angle_a, angle_b, angle_c):
+    position = position.Position()
+
+
+def run(self):
+    #   doe dit
+    calc_position(self, )
+
+
+
+if __name__ == "__main__":
+    b1 = Beacon(b1x, b1y, angle_a, "oorsprong")
+    b2 = Beacon(b2x, b2y, angle_b, "rechts")
+    b3 = Beacon(b3x, b3y, angle_c, "rechtsboven")
